54643c534f0621384c5f5679cc3998d7d1eda719
Ingrid Silapan <ingrid.silapan@sushmobile.com>
2018-08-27 10:40:26 +1200

    Initial Commit

---
+++ UserInactivityTimeout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserInactivityTimeout
+//
+//  Created by Ingrid Silapan on 27/08/18.
+//  Copyright © 2018 irs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
